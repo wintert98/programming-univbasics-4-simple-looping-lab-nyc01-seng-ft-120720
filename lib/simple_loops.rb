16e72fc1cc103aa878c42a6ed9a2d617b4285e4f
Thomas Winter <wintert98@gmail.com>
2020-11-25 19:37:22 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -14,3 +14,11 @@ def loop_message_n_times(message, num)
   counter += 1
   end
 end
+
+def output_array(array)
+  counter = 0 
+  while counter < array.length 
+  puts array[counter]
+  counter += 1
+  end
+end
