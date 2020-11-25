672da76ec4c6d01597c0a61f77bf82922f9e2c8f
Thomas Winter <wintert98@gmail.com>
2020-11-25 19:33:37 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -6,3 +6,11 @@ def loop_message_five_times(message)
   counter += 1
   end
 end
+
+def loop_message_n_times(message, num)
+  counter = 0 
+  while counter <= num
+  puts message
+  counter += 1
+  end
+end
